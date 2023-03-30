a4a02902d4d75e6090894b1697b0176ebbd4c7ce
AryanKathpal <aryankathpal.cse19@chitkarauniversity.edu.in>
2023-03-30 14:55:03 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PurchasedComponent } from './components/purchased/purchased.component';
+import { ItemsComponent } from './components/items/items.component';
+import { SalesComponent } from './components/sales/sales.component';
+import { StocksComponent } from './components/stocks/stocks.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SuplierComponent } from './components/suplier/suplier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    PurchasedComponent,
+    ItemsComponent,
+    SalesComponent,
+    StocksComponent,
+    SidebarComponent,
+    SuplierComponent
   ],
   imports: [
     BrowserModule,
